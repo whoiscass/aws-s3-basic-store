78e4bf3f192c6702a880f2c98bc873aa021628fe
hepolanco <hctr.kz@gmail.com>
2020-11-28 13:42:14 -0500

    update

--- src/main/java/com/cass/aws/upload/fileStoreService/IFilesStoreService.java
+++ src/main/java/com/cass/aws/upload/fileStoreService/IFilesStoreService.java
@@ -5,7 +5,7 @@ import java.util.Map;
 import java.util.Optional;
 
 public interface IFilesStoreService {
-    public String uploadFile(String fileName,
+    String uploadFile(String fileName,
                              Optional<Map<String,String>> optionalMetadata,
                              InputStream inputStream);
 }
